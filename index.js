89c4a1acccfd8b5ba21f07212c267ef69e4649bc
Jessica Kim <jess2963@gmail.com>
2018-05-30 18:20:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ var height = 38
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+
+var word
+word = 'bird'
+console.log(word)
+word = 'eight'
+console.log(word)
